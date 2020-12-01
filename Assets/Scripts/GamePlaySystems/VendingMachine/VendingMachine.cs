0c473204d8b812b2944e2edada1d5e47259e034d
RedWraith <brianpurdy01@gmail.com>
2020-12-01 00:39:58 -0500

    loading screen meta file

--- Assets/Scripts/GamePlaySystems/VendingMachine/VendingMachine.cs
+++ Assets/Scripts/GamePlaySystems/VendingMachine/VendingMachine.cs
@@ -68,6 +68,7 @@ public class VendingMachine : MonoBehaviour, IVendingMachine
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
                 //*******this needs to be updated for the UI is attached, hard coding first Object******
                 SpawnSoldItem(0);
 =======
@@ -98,6 +99,10 @@ public class VendingMachine : MonoBehaviour, IVendingMachine
 
                 SpawnSoldItem(-1);
 >>>>>>> parent of 4754e8e... Merge branch 'main' of https://github.com/owenhooper/SpinCycle into main
+=======
+                //*******this needs to be updated for the UI is attached, hard coding first Object******
+                SpawnSoldItem(0);
+>>>>>>> parent of 418990b... Evan branch (#6)
             }
         }
         
