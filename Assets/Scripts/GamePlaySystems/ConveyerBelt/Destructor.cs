7ae28edd09ca133ce9fc3ffda912fb70d4de81e6
RedWraith <brianpurdy01@gmail.com>
2020-12-17 15:27:56 -0500

    Connected the items value to the Points system

--- Assets/Scripts/GamePlaySystems/ConveyerBelt/Destructor.cs
+++ Assets/Scripts/GamePlaySystems/ConveyerBelt/Destructor.cs
@@ -1,14 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Destructor : MonoBehaviour
 {
+    // THIS IS FOR TESTING ONLY
+    public Text pointsText = null;
+
     // Start is called before the first frame update
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Item")
         {
+            // THIS IS FOR TESTING ONLY
+            pointsText.text = (Convert.ToInt32(pointsText.text) + collision.gameObject.GetComponent<TShirt>().Price).ToString();
+        
+        
             Destroy(collision.gameObject);
         }
     }
