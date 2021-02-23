1ace6fb8b1bb1cc7a9f6669f2003571c84b5d0ae
Evan-peps <evan.peppler@torontofilmschool.ca>
2021-02-23 12:53:33 -0500

    Mixamo test and repair tool
    Mixamo test stuff and the repair tool respawn stuff

--- Assets/Scripts/GamePlaySystems/Tools/RepairToolSpawn.cs
+++ Assets/Scripts/GamePlaySystems/Tools/RepairToolSpawn.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class RepairToolSpawn : MonoBehaviour
 {
+    public GameObject repairTool;
+    public GameObject spawnPoint;
+    private Vector3 spawnPointPosition;
+    public GameObject repairToolInstance;
+    public float spawnTimer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnPointPosition = spawnPoint.transform.position;
+        Instantiate(repairTool, spawnPointPosition, Quaternion.identity);
+        spawnTimer += 3;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      if (GameObject.Find("RepairTool(Clone)") == null)
+        {
+            spawnTimer -= Time.deltaTime;
+            if (spawnTimer <= 0)
+            {
+                spawnPointPosition = spawnPoint.transform.position;
+                Instantiate(repairTool, spawnPointPosition, Quaternion.identity);
+                spawnTimer += 3;
+            }
+
+        }
+
+
     }
+
+
 }
