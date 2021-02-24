b0e30a57c755a9ada1f4f2a132d2f114f342952c
Evan-peps <evan.peppler@torontofilmschool.ca>
2021-02-24 16:03:35 -0500

    RepairToolDoneForNow
    just gotta let it repair the machines

--- Assets/Scripts/GamePlaySystems/Tools/RepairToolSpawn.cs
+++ Assets/Scripts/GamePlaySystems/Tools/RepairToolSpawn.cs
@@ -6,34 +6,67 @@ public class RepairToolSpawn : MonoBehaviour
 {
     public GameObject repairTool;
     public GameObject spawnPoint;
-    private Vector3 spawnPointPosition;
+    public Vector3 spawnPointPosition;
     public GameObject repairToolInstance;
     public float spawnTimer;
+    public List<GameObject> repairTools;
+    public static RepairToolSpawn instance { get; private set; }
     // Start is called before the first frame update
     void Start()
     {
+        if(instance)
+        {
+            Destroy(this);
+        }
+        instance = this;
         spawnPointPosition = spawnPoint.transform.position;
-        Instantiate(repairTool, spawnPointPosition, Quaternion.identity);
+        repairToolInstance = Instantiate(repairTool, spawnPointPosition, Quaternion.identity);
+        repairToolInstance.GetComponent<RepairToolUse>().spawner = transform.position;
         spawnTimer += 3;
+        repairTools.Add(repairToolInstance);
     }
 
     // Update is called once per frame
     void Update()
     {
-      if (GameObject.Find("RepairTool(Clone)") == null)
+
+        repairToolInstance = GameObject.Find("RepairTool(Clone)");
+
+        if (repairToolInstance == null)
         {
             spawnTimer -= Time.deltaTime;
             if (spawnTimer <= 0)
             {
                 spawnPointPosition = spawnPoint.transform.position;
                 Instantiate(repairTool, spawnPointPosition, Quaternion.identity);
-                spawnTimer += 3;
+                spawnTimer = 3;
+                repairTools.Add(repairToolInstance);
             }
 
         }
-
-
+        else if (Vector3.Distance(repairToolInstance.transform.position, spawnPointPosition) >= 1)
+        {
+            if (repairTools.Count <= 1)
+            {
+                spawnTimer -= Time.deltaTime;
+                if (spawnTimer <= 0)
+                {
+                    spawnPointPosition = spawnPoint.transform.position;
+                    Instantiate(repairTool, spawnPointPosition, Quaternion.identity);
+                    spawnTimer = 3;
+                    repairTools.Add(repairToolInstance);
+                }
+            }
+        }
+        else
+        {
+            Debug.Log(repairTools.Count);
+        }
     }
 
+    public void RemoveObject()
+    {
+       repairTools.Remove(repairToolInstance);
+    }
 
 }
