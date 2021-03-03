854a708fffb60e7de5dae1ee56443a3cfc1a3b07
AnthonyRinaldiDelio <65902280+AnthonyRinaldiDelio@users.noreply.github.com>
2021-03-02 22:00:45 -0500

    Updated grab scripted (#33)
    fixed water gun and updated the grab scripted

---
+++ Assets/Scripts/Items/BulletScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+
+    
+{
+
+    private Collider collider;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Machine") || collision.gameObject.CompareTag("VendingMachine"))
+        {
+            Physics.IgnoreCollision(collision.collider, collider);
+        }
+    }
+
+    private void Start()
+    {
+        collider = GetComponent<Collider>();
+    }
+
+}
