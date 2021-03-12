5ce5f7430dfe9f36921b95bead6966c1cf682145
owenhooper <owenhooper@gmail.com>
2021-03-12 00:29:10 -0500

    Owen physics (#40)
    * updated gunplay
    
    worked with weapon script, grab script and cinemachine
    
    * Fixed camera and added transparency mask
    
    starting to come together

---
+++ Assets/Scripts/GamePlaySystems/Level/TransparentWalls.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransparentWalls : MonoBehaviour
+{
+    public Material[] material;
+    private bool transparent = false;
+    private MeshRenderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<MeshRenderer>();
+
+
+    }
+
+    public void ChangeTransparency(bool transparent)
+    {
+        if (this.transparent == transparent) return;
+
+        this.transparent = transparent;
+
+        if (transparent)
+        {
+            rend.material = material[1];
+        }
+        else
+        {
+            rend.material = material[0];
+        }
+
+
+    }
+}
