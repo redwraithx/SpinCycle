f8da22bfb80f491d21a5b7229e9a73397f747068
Brian Purdy <brianpurdy01@gmail.com>
2021-04-14 09:58:04 -0400

    Patched a bunch of errors see the Description (#65)
    - Fixed network manager & audio manager errors on restarting a new game from an old game.
    - Fixed grab script falling through world should be fixed now.
    - Fixed errors with returning to menu on disconnection of player(s)

--- Assets/Scripts/CharacterMechanics/Grab.cs
+++ Assets/Scripts/CharacterMechanics/Grab.cs
@@ -50,9 +50,13 @@ public class Grab : MonoBehaviour
             if(itemInHand.GetComponent<ItemTypeForItem>())
                 itemInHand.GetComponent<ItemTypeForItem>().RequestOwnership();
 
+            
+            
             foreach (var itemCollider in itemInHand.GetComponents<Collider>())
             {
-                    itemCollider.enabled = false;
+                Physics.IgnoreCollision(GetComponent<Collider>(), itemCollider, true);
+                
+                    //itemCollider.enabled = false;
             }
             
             itemInHand.GetComponent<Rigidbody>().useGravity = false;
@@ -76,7 +80,8 @@ public class Grab : MonoBehaviour
 
         foreach (var itemCollider in itemInHand.GetComponents<Collider>())
         {
-                itemCollider.enabled = true;
+            Physics.IgnoreCollision(GetComponent<Collider>(), itemCollider, false);
+                //itemCollider.enabled = true;
         }
         
         itemInHand.GetComponent<Rigidbody>().useGravity = true;
@@ -210,7 +215,9 @@ public class Grab : MonoBehaviour
 
         foreach (var itemCollider in itemInHand.GetComponents<Collider>())
         {
-            itemCollider.enabled = true;
+            Physics.IgnoreCollision(GetComponent<Collider>(), itemCollider, false);
+
+            //itemCollider.enabled = true;
         }
         
         itemInHand.GetComponent<Rigidbody>().useGravity = true;
