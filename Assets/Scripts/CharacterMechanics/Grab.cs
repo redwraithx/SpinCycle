e03fd6297217c220b768780faf503d64922066c7
Evan-peps <evan.peppler@torontofilmschool.ca>
2021-07-27 10:58:04 -0400

    Laundry Basket Networked
    tada

--- Assets/Scripts/CharacterMechanics/Grab.cs
+++ Assets/Scripts/CharacterMechanics/Grab.cs
@@ -56,7 +56,8 @@ public class Grab : MonoBehaviour
             {
                     itemCollider.enabled = false;
             }
-            
+
+            itemInHand.GetComponent<Item>().OwnerID = this.gameObject.GetComponent<PhotonView>().ViewID;
             itemInHand.GetComponent<Rigidbody>().useGravity = false;
             itemInHand.transform.position = grabPoint.position;
             
