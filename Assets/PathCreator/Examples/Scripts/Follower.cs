15ec9526abf5be4e3760e90fa9f730c99a862ab0
AnthonyRinaldiDelio <65902280+AnthonyRinaldiDelio@users.noreply.github.com>
2021-07-28 13:07:07 -0400

    Spline
    Spline

---
+++ Assets/PathCreator/Examples/Scripts/Follower.cs
@@ -0,0 +1,16 @@
+﻿using PathCreation;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+
+    public PathCreator pathCreator;
+    public float speed = 10;
+    float distanceTravelled;
+
+    private void Update()
+    {
+        distanceTravelled += speed * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
+    }
+}
