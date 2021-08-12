b46b4fc85fe766f19b1ab38cd806de1d6bd05c41
Evan-peps <68343486+Evan-peps@users.noreply.github.com>
2021-08-12 16:04:47 -0400

    Evan branch again (#135)
    * Machines and Level
    
    Machines are now in level and I've adjusted the colliders to fit the models, level also has colliders and should work
    
    * bunch of stuff
    
    new script for item deletion, and the item stand itself now has a placeholder spawner to sync the new spawn time and text that says what the object that spawned is

---
+++ Assets/Scripts/Items/ItemDeletion.cs
@@ -0,0 +1,29 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDeletion : MonoBehaviour
+{
+
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer += 30;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > 0 && this.transform.parent == null && this.gameObject.GetComponent<Item>().OwnerID != 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        if(timer <= 0 && this.transform.parent == null && this.gameObject.GetComponent<Item>().OwnerID != 0)
+        {
+            Debug.Log("Bye Bye" + this.gameObject.name);
+            PhotonNetwork.Destroy(this.gameObject);
+        }
+    }
+}
