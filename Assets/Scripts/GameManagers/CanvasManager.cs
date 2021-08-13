4d469e7f4ef6353ed90138d4cff4c78435862ab6
RedWraith <brianpurdy01@gmail.com>
2021-08-12 22:22:18 -0400

    Tutorial onfirst time run
    - Added check for tutorial first time played
    - Code tested successfully, Code is commented out until tutorial level has been completed.
    - Still need to add tutorial was completed for current player.

--- Assets/Scripts/GameManagers/CanvasManager.cs
+++ Assets/Scripts/GameManagers/CanvasManager.cs
@@ -1,6 +1,11 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
+using PlayerProfileData;
+using NetworkProfile;
+using TMPro;
 
 public class CanvasManager : MonoBehaviour
 {
@@ -11,16 +16,62 @@ public class CanvasManager : MonoBehaviour
     public Button settingsButton;
 
     public int MainMenuSceneIndex = 0;
-    
+
+    [SerializeField] private int mainMenuSceneBuildIndex = 1;
+    [SerializeField] private bool hasPlayerProfileLoaded = false;
     
     void Start()
     {
+        Debug.Log(this.name + " has loaded start()");
+        
         Cursor.lockState = CursorLockMode.None;
         
         //these are all just checking to see if you've hit the button and then triggering the scripts in game manager
         if (startButton)
         {
-            startButton.onClick.AddListener(GameManager.Instance.StartGame);
+            Debug.Log("start button does exist");
+            // This is for checking for tutorial has been played or not
+            // if (SceneManager.GetActiveScene().buildIndex == mainMenuSceneBuildIndex)
+            // {
+            //     // load the player profile
+            //     ProfileData profile = DataClass.LoadProfile();
+            //     
+            //     hasPlayerProfileLoaded = true;
+            //     
+            //     if(profile != null)
+            //     {
+            //         Debug.Log("player profile was loaded and is NOT null");
+            //         
+            //         if (profile.hasPlayedTutorial == false)
+            //         {
+            //             Debug.Log("player profile shows that the tutorial level has not been played yet.");
+            //                 
+            //             var startButtonComponentTMP = startButton.GetComponentInChildren<TMP_Text>();
+            //
+            //             //startButtonComponentTMP.text = "Learn To Play";
+            //             
+            //             startButton.onClick.AddListener(GameManager.Instance.PlayTutorial);
+            //         }
+            //         else
+            //         {
+            //             Debug.Log("players profile show that the tutorial was already played");
+            //             
+            //             startButton.onClick.AddListener(GameManager.Instance.StartGame);
+            //         }
+            //     }
+            //     else
+            //     {
+            //         Debug.Log("Players profile has not been created or has become corrupted");
+            //         
+            //         startButton.onClick.AddListener(GameManager.Instance.StartGame);
+            //     }
+            // }
+            // else
+            // {
+            //     Debug.Log("did not load player profile as we are not on the main menu scene.");
+            //     
+                 startButton.onClick.AddListener(GameManager.Instance.StartGame);
+            // }
         }
 
         if (quitButton)
@@ -38,6 +89,26 @@ public class CanvasManager : MonoBehaviour
         if (settingsButton)
         {
             settingsButton.onClick.AddListener(GameManager.Instance.ToSettings);
+        }
+    }
+
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == mainMenuSceneBuildIndex)
+        {
+            // load the player profile
+            
+            
+            // check if player has seen the tutorial
+            
+            
+            // show the tutorial if they have not seen it yet.
+            
+            if (playButton)
+            {
+                playButton.onClick.AddListener(GameManager.Instance.PlayGame);
+            }
+            
         }
     }
 
