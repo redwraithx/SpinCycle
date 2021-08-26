8f983f43c2340d32dcf3032c5c85bd6429ae104b
Evan-peps <68343486+Evan-peps@users.noreply.github.com>
2021-08-26 17:09:50 -0400

    Credits and UI Fixes (#161)
    Updating from main and building after this

---
+++ Assets/CreditsScroll.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class CreditsScroll : MonoBehaviour
+{
+    public GameObject creditsScroll;
+    public GameObject creditsBottom;
+    public GameObject creditsTop;
+    public Vector3 creditsTopInitLocation;
+    public Vector3 creditsBottomLocation;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        creditsTopInitLocation = creditsTop.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKey)
+        {
+            SceneManager.LoadScene("MainMenuScene");
+        }
+
+        creditsBottomLocation = creditsBottom.transform.position;
+
+        if(Mathf.Abs(Vector3.Distance(creditsTopInitLocation, creditsBottomLocation)) >= 50)
+        {
+            creditsScroll.transform.Translate(Vector3.up * speed);
+        }
+
+    }
+}
