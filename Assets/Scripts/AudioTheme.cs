1c5000db894e940b125be930f1389456a68f7036
AnthonyRinaldiDelio <65902280+AnthonyRinaldiDelio@users.noreply.github.com>
2021-09-13 02:46:56 -0400

    Audio
    Audio for player and theme

---
+++ Assets/Scripts/AudioTheme.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class AudioTheme : MonoBehaviour
+{
+    //public int netWorkLobbySceneIndex;
+    public static AudioTheme instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        AudioClip gameTheme = Resources.Load<AudioClip>("AudioFiles/Music/MenuTheme/Washed_Up_theme_idea_1(1)");
+        GameManager.audioManager.PlayMusic(gameTheme);
+        //DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+  /*  void Update()
+    {
+        if(SceneManager.GetActiveScene().buildIndex == netWorkLobbySceneIndex)
+        {
+            Debug.Log("Not Stopingasdjkahdskajhdsj");
+            
+
+            if (GameManager.audioManager.IsMusicPlaying())
+            {
+                GameManager.audioManager.StopMusic();
+            }
+        }
+    }*/
+}
