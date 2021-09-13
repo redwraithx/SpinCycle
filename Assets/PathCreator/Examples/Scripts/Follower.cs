09459f79460278688d399dd04298454297592a28
AnthonyRinaldiDelio <65902280+AnthonyRinaldiDelio@users.noreply.github.com>
2021-09-13 02:08:45 -0400

    Spines
    Spines

--- Assets/PathCreator/Examples/Scripts/Follower.cs
+++ Assets/PathCreator/Examples/Scripts/Follower.cs
@@ -1,16 +1,45 @@
 ﻿using PathCreation;
+using System.Collections;
 using UnityEngine;
+
+using Random = UnityEngine.Random;
 
 public class Follower : MonoBehaviour
 {
 
     public PathCreator pathCreator;
-    public float speed = 10;
-    float distanceTravelled;
+    public float speed = 0f;
+    float distanceTravelled = 0f;
 
+    /* private IEnumerator SplineTubs()
+     {
+
+         yield return new WaitForSeconds(5f);
+         Debug.Log("Playing");
+         StartCoroutine(SplineTubs());
+     }*/
     private void Update()
     {
-        distanceTravelled += speed * Time.deltaTime;
         transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
+        distanceTravelled += speed * Time.deltaTime;
+    }
+    private void Start()
+    {
+        //RandomNumber();
+        //StartCoroutine("SplineTubs");
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ball")
+        {
+            RandomNumber();
+        }
+    }
+
+    public void RandomNumber()
+    {
+        speed = Random.Range(2f, 6f);
     }
 }
---
+++ Assets/Scripts/FolderBelt.cs
@@ -0,0 +1,34 @@
+﻿using PathCreation;
+using UnityEngine;
+
+public class FolderBelt : MonoBehaviour
+{
+
+    public PathCreator pathCreator;
+    public float speed;
+    float distanceTravelled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gameObject.CompareTag("ConveyorObject"))
+        {
+            pathCreator = GetComponentInParent<PathCreator>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartBelt();
+
+    }
+        
+
+    public void StartBelt()
+    {
+            distanceTravelled += speed * Time.deltaTime;
+            transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
+        
+    }
+}
