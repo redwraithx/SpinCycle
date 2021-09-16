b83a96c0b8efb9f40edc255d784284a6c8fcf504
owenhooper <owenhooper@gmail.com>
2021-09-16 18:20:46 -0400

    Freeze gun is sick now (#217)
    * Fixed animation sync, player grab and some other stuff
    
    Getting there
    
    * More VFX mostly
    
    Still need to work out why the freezegun is only instantiationg the snowball wtihout particle effect
    
    * Added dryer animation, tweaked intro camera
    
    * Alembics now working, redid camera
    
    who keep messing with my camera
    
    * Properly consolidated changes with final level
    
    scooby dooby doo
    
    * Ice gun fixes
    
    * Consolidated FinalLevel changes
    
    you can just replace final level with this one, it's exactly the same but i added the alembics and camera dolly
    
    * Redid overwritten stuff and fixed freeze gun (again)
    
    what's a guy gotta do to have his work saved around these parts xD
    
    * Freeze Gun works way better now
    
    yay

--- Assets/Scripts/Items/IceCube.cs
+++ Assets/Scripts/Items/IceCube.cs
@@ -14,11 +14,14 @@ public class IceCube : MonoBehaviourPun
     public BoxCollider boxCollider;
 
     bool spawnedPatch = false;
+    bool playingAudio = false;
+
+    //public GameObject icePatch;
     // Start is called before the first frame update
     void Start()
     {
-        boxCollider.enabled = false;
-        Invoke("StartSphereCast", 0.3f);
+        //boxCollider.enabled = false;
+/*        Invoke("StartSphereCast", 0.02f);*/
     }
 
     // Update is called once per frame
@@ -39,48 +42,62 @@ public class IceCube : MonoBehaviourPun
 
     private void OnCollisionEnter(Collision collision)
     {
-        AudioClip freezeCollison = Resources.Load<AudioClip>("AudioFiles/SoundFX/Sabotages/FreezeGun/freeze");
-        GameManager.audioManager.PlaySfx(freezeCollison);
-        RaycastHit[] hits = Physics.SphereCastAll(transform.position, 4.0f, transform.forward, 1.0f, layerMask, QueryTriggerInteraction.UseGlobal);
-        foreach (RaycastHit hit in hits)
+        if(collision.gameObject.tag != "Item")
         {
-            if (spawnedPatch)
+            if(playingAudio == false)
             {
-                break;
+                AudioClip freezeCollison = Resources.Load<AudioClip>("AudioFiles/SoundFX/Sabotages/FreezeGun/freeze");
+                GameManager.audioManager.PlaySfx(freezeCollison);
+                playingAudio = true;
             }
 
-            Debug.Log("hit");
-            if (hit.transform.gameObject.tag == "Player")
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, 4);
+            //RaycastHit[] hits = Physics.SphereCastAll(transform.position, 4.0f, transform.forward, 1.0f, layerMask, QueryTriggerInteraction.UseGlobal);
+
+            foreach (var hitCollider in hitColliders)
             {
-                hit.transform.gameObject.GetComponent<PlayerMovementCC>().isFrozen = true;
+
+                Debug.Log("hit");
+                if (hitCollider.transform.gameObject.tag == "Player")
+                {
+                    /*foreach(GameObject player in GameManager.networkLevelManager.playersJoined)
+                    {
+                        if(hitCollider.transform.gameObject == player)
+                        {
+                            player.transform.gameObject.GetComponent<PlayerMovementCC>().isFrozen = true;
+                        }
+                    }*/
+
+                    foreach (GameObject player in GameManager.networkLevelManager.playersJoined)
+                    {
+                        if (player.GetComponent<PhotonView>().ViewID == hitCollider.transform.gameObject.GetComponent<PhotonView>().ViewID)
+                        {
+                            player.GetComponent<PlayerMovementCC>().isFrozen = true;
+                        }
+                    }
+/*                    hitCollider.transform.gameObject.GetPhotonView();
+                    hitCollider.transform.gameObject.GetComponent<PlayerMovementCC>().isFrozen = true;*/
+
+                }
+
+            }
+
+            //Instantiate(icePatch, transform.position, transform.rotation);
+            if (PhotonNetwork.IsMasterClient)
+            {
                 if (spawnedPatch == false)
                 {
                     //GetComponentInChildren<VFX_IceBullet_DissolveIn>().InsantiateCollideVFX(collision);
                     PhotonNetwork.Instantiate(Path.Combine("PhotonItemPrefabs", "IcePatch"), transform.position, transform.rotation);
                     spawnedPatch = true;
                 }
-                
-            }
-            
-            
-
-            else if (hit.transform.gameObject.layer == 8)
-            {
-                if(PhotonNetwork.IsMasterClient)
-                {
-                    if(spawnedPatch == false)
-                    {
-                        //GetComponentInChildren<VFX_IceBullet_DissolveIn>().InsantiateCollideVFX(collision);
-                        PhotonNetwork.Instantiate(Path.Combine("PhotonItemPrefabs", "IcePatch"), transform.position, transform.rotation);
-                        spawnedPatch = true;
-                    }
-
-                }
 
             }
+            GetComponent<PhotonView>().TransferOwnership(PhotonNetwork.MasterClient);
+            PhotonNetwork.Destroy(gameObject);
+            //Destroy(gameObject);
         }
-        GetComponent<PhotonView>().TransferOwnership(PhotonNetwork.MasterClient);
-        PhotonNetwork.Destroy(gameObject);
+
     }
 
     private void OnDrawGizmosSelected()
@@ -94,6 +111,8 @@ public class IceCube : MonoBehaviourPun
     {
         boxCollider.enabled = true;
     }
+
+
 
    
 }
